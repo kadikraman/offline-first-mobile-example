40a97fa4602905a4477267141fc80a717ec4cf4b
Kadi Kraman <kadi.kraman@formidable.com>
2018-10-14 15:10:47 +0100

    Add a Apollo link retry

--- app/src/App.js
+++ app/src/App.js
@@ -5,11 +5,30 @@ import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { RetryLink } from 'apollo-link-retry';
+import { ApolloLink } from 'apollo-link';
 
 import RootComponent from './screens/RootComponent';
 
+const retryLink = new RetryLink({
+  delay: {
+    initial: 500
+  },
+  attempts: {
+    max: 100,
+    retryIf: (error, _operation) => {
+      console.log('retrying');
+      return true;
+    }
+  }
+});
+
+const httpLink = new HttpLink({ uri: 'http://localhost:7000/graphql' });
+
+const link = ApolloLink.from([retryLink, httpLink]);
+
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:7000/graphql' }),
+  link,
   cache: new InMemoryCache()
 });
 
